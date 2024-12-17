b2e1bb92440d3e8dd7c27840265cc8655b10711f
elivaly <vetashapel@gmail.com>
2024-12-17 16:53:20 +0300

    start

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Server.Database;
+
+using (DBC db = new DBC()) 
+{
+    customers customer1 = new customers { name = "Luis", password = "French", description = "Client"};
+    customers customer2 = new customers { name = "Anya", password = "bert1234", description = "Guest" };
+
+    db.customers.AddRange(customer1, customer2);
+    db.SaveChanges();
+}
+using (DBC db = new DBC()) 
+{
+    var users = db.customers.ToList();
+    Console.WriteLine("All users in database:");
+    foreach(var user in users) 
+    {
+        Console.WriteLine($"{user.id} {user.name} - Пароль:{user.password}, Категория:{user.description}");
+    }
+}
