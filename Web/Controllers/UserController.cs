9b63be15e54191f5374e8cef7af6068a77ada07f
elivaly <vetashapel@gmail.com>
2024-12-18 10:07:44 +0300

    task

--- Web/Controllers/UserController.cs
+++ Web/Controllers/UserController.cs
@@ -1,6 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using AuthService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Web.Controllers
 {
@@ -10,10 +13,57 @@ namespace Web.Controllers
     {
         [HttpGet]
         [Route("GetByName")]
-        public IActionResult GetByName([FromQuery][Required] string name) 
+        public async Task<IActionResult> GetByName([FromQuery][Required] string name) 
         {
-            return Ok("hello");
+            if (string.IsNullOrEmpty(name)) 
+            { 
+                return BadRequest("Name is required."); 
+            }
+            List<string> users = new List<string>();
+            await using (DBC dBC = new DBC())
+            {
+                users = await dBC.customers
+                    .Where(x => x.name == name)
+                    .Select(x => x.name)
+                    .Distinct()
+                    .ToListAsync();
+            };
+            return Ok(users);
         }
-        
+
+        [HttpGet]
+        [Route("GetByDesc")]
+        public async Task<IActionResult> GetByDesc([FromQuery][Required] string desc)
+        {
+            if (string.IsNullOrEmpty(desc)) 
+            { 
+                return BadRequest("Description is required."); 
+            }
+            List <string> users = new List<string>();
+            await using (DBC dBC = new DBC())
+            {
+                users = await dBC.customers
+                    .Where(x => x.description == desc)
+                    .Select(x => x.name)
+                    .Distinct()
+                    .ToListAsync();
+            };
+            return Ok(users);
+        }
+
+        [HttpGet]
+        [Route("GetAllUsers")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            List<string> users = new List<string>();
+            await using (DBC dBC = new()) 
+            { 
+                users = await dBC.customers
+                    .Select(x => x.name)
+                    .Distinct()
+                    .ToListAsync(); 
+            };
+            return Ok(users);
+        }
     }
 }
